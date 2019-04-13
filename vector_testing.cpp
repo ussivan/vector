ae9474fb62d6a70f5c63a41bc232a7aa4d74393e
Ivan Sorokin <vanyacpp@gmail.com>
2019-04-13 14:55:48 +0300

    push_back test

--- vector_testing.cpp
+++ vector_testing.cpp
@@ -7,5 +7,22 @@ TEST(correctness, default_ctor)
 {
     container c;
     EXPECT_EQ(0u, c.size());
+    EXPECT_TRUE(c.empty());
 }
 
+TEST(correctness, push_back)
+{
+    container c;
+    
+    for (size_t i = 0; i != 1000; ++i)
+    {
+        int val = (i + 42) % 13;
+        c.push_back(val);
+    }
+
+    for (size_t i = 0; i != 1000; ++i)
+    {
+        int val = (i + 42) % 13;
+        EXPECT_EQ(val, c[i]);
+    }
+}
