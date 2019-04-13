073d4d821842d471b9926f5ce0fecf64b216edc0
Ivan Sorokin <vanyacpp@gmail.com>
2019-04-13 14:57:28 +0300

    one more test

--- vector_testing.cpp
+++ vector_testing.cpp
@@ -10,7 +10,30 @@ TEST(correctness, default_ctor)
     EXPECT_TRUE(c.empty());
 }
 
-TEST(correctness, push_back)
+TEST(correctness, push_back_simple)
+{
+    container c;
+    c.push_back(4);
+    EXPECT_FALSE(c.empty());
+    EXPECT_EQ(1u, c.size());
+    c.push_back(8);
+    EXPECT_FALSE(c.empty());
+    EXPECT_EQ(2u, c.size());
+    c.push_back(15);
+    EXPECT_FALSE(c.empty());
+    EXPECT_EQ(3u, c.size());
+    c.push_back(16);
+    EXPECT_FALSE(c.empty());
+    EXPECT_EQ(4u, c.size());
+    c.push_back(23);
+    EXPECT_FALSE(c.empty());
+    EXPECT_EQ(5u, c.size());
+    c.push_back(42);
+    EXPECT_FALSE(c.empty());
+    EXPECT_EQ(6u, c.size());
+}
+
+TEST(correctness, push_back_big)
 {
     container c;
     
